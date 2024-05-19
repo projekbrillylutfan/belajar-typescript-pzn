18056e9856a6f3e884c3a5c4d6a2710f757e3eae
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-05-19 16:37:34 +0700

    validation sudah selesai

--- belajar-typescript-validation/test/1_schema-create.test.ts
+++ belajar-typescript-validation/test/1_schema-create.test.ts
@@ -80,45 +80,162 @@ describe("zod", () => {
   });
 
   it("should can validate object", () => {
-      const loginSchema = z.object({
-          username: z.string().email(),
-          password: z.string().min(6).max(100),
-      })
+    const loginSchema = z.object({
+      username: z.string().email(),
+      password: z.string().min(6).max(100),
+    });
 
-      const request = {
-          username: "a@a.com",
-          password: "123456",
-          ignore: "ignore"
-      }
+    const request = {
+      username: "a@a.com",
+      password: "123456",
+      ignore: "ignore",
+    };
 
-      const result = loginSchema.parse(request);
-      console.info(result);
-  })
+    const result = loginSchema.parse(request);
+    console.info(result);
+  });
 
   it("should can nested object", () => {
     const createUserSchema = z.object({
-        id: z.string().max(100),
-        name: z.string().min(3).max(100),
-        address: z.object({
-            street: z.string().max(100),
-            city: z.string().max(100),
-            zip: z.string().max(10),
-            country: z.string().max(100),
-        })
-    })
+      id: z.string().max(100),
+      name: z.string().min(3).max(100),
+      address: z.object({
+        street: z.string().max(100),
+        city: z.string().max(100),
+        zip: z.string().max(10),
+        country: z.string().max(100),
+      }),
+    });
 
     const request = {
-        id: "123",
-        name: "Eko",
-        address: {
-            street: "jalan raya",
-            city: "jakarta",
-            zip: "12345",
-            country: "indonesia"
-        }
-    }
+      id: "123",
+      name: "Eko",
+      address: {
+        street: "jalan raya",
+        city: "jakarta",
+        zip: "12345",
+        country: "indonesia",
+      },
+    };
 
     const result = createUserSchema.parse(request);
     console.info(result);
-})
+  });
+
+  it("should can validate array", () => {
+    const schema = z.array(z.string()).min(1).max(10);
+
+    const request: Array<string> = ["a", "b", "c"];
+    const result: Array<string> = schema.parse(request);
+
+    console.log(result);
+  });
+
+  it("should can validate set", () => {
+    const schema = z.set(z.string()).min(1).max(10);
+
+    const request: Set<string> = new Set(["a", "b", "c"]);
+    const result: Set<string> = schema.parse(request);
+
+    console.log(result);
+  })
+
+  it("should can validate map", () => {
+    const schema = z.map(z.string(), z.string());
+
+    const request: Map<string, string> = new Map([
+      ["a", "b"],
+      ["c", "d"],
+    ]);
+    const result: Map<string, string> = schema.parse(request);
+
+    console.log(result);
+  })
+
+  it('should can validate object with message', () => {
+    const loginSchema = z.object({
+        username: z.string().email("usernmae harus email"),
+        password: z.string().min(6, "password minimal 6 karakter").max(20, "password maksimal 20 karakter")
+    })
+
+    const request = {
+        username: "a@",
+        password: "12",
+        ignore: "ignore",
+    }
+
+    try {
+        loginSchema.parse(request)
+    } catch(e) {
+        if (e instanceof ZodError) {
+            console.info(e.errors)
+            e.errors.forEach((error) => {
+                console.info(error.message)
+            })
+        }
+    }
+  })
+
+  it("should can support optional validation", () => {
+    const registerSchema = z.object({
+        username: z.string().email(),
+        password: z.string().min(6).max(20),
+        firstName: z.string().min(3).max(100),
+        lastName: z.string().min(3).max(100).optional(),   
+    })
+
+    const request = {
+        username: "a@a.com",
+        password: "123456",
+        firstName: "Eko",
+        lastName: "Kurniawan",
+        ignore: "ignore",
+    }
+
+    const result = registerSchema.parse(request);
+    console.info(result);
+  })
+
+  it("should can support transform validation", () => {
+    const schema = z.string().email().transform((value) => {
+        return value.toUpperCase();
+    })
+
+    const result = schema.parse("eko@gmail.com");
+    console.log(result);
+  })
+
+  it("should can create custom validation", () => {
+    const loginSchema = z.object({
+        username: z.string().email().transform((data, ctx) => {
+            if (data !== data.toUpperCase()) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "username harus uppercase",
+                })
+                return z.NEVER
+            } else {
+                return data
+            }
+        }),
+        password: z.string().min(6, "password minimal 6 karakter").max(20, "password maksimal 20 karakter")
+    })
+
+    const request = {
+        username: "eko@gmail.com",
+        password: "123456",
+        ignore: "ignore",
+    }
+
+    try {
+        loginSchema.parse(request)
+    } catch(e) {
+        if (e instanceof ZodError) {
+            console.info(e.errors)
+            e.errors.forEach((error) => {
+                console.info(error.message)
+            })
+        }
+    }
+  })
 });
