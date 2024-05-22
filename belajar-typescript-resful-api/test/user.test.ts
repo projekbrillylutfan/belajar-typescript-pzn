0a19dda063de2fe6e09c08880164121460c0b77c
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-05-22 19:00:36 +0700

    test register selesai

--- belajar-typescript-resful-api/test/user.test.ts
+++ belajar-typescript-resful-api/test/user.test.ts
@@ -1,8 +1,13 @@
 import supertest from "supertest"
 import { web } from "../src/application/web"
 import { logger } from "../src/application/logging"
+import UserTest from "./test-util"
 
 describe("POST /api/users", () => {
+
+    afterEach(async () => {
+        await UserTest.delete()
+    })
     it("should reject register new user if username already exists", async () => {
         const response = await supertest(web)
             .post("/api/users")
@@ -15,5 +20,20 @@ describe("POST /api/users", () => {
         console.log(response)
         expect(response.status).toBe(400)
         expect(response.body.errors).toBeDefined()
+    })
+
+    it("should register new user", async () => {
+        const response = await supertest(web)
+            .post("/api/users")
+            .send({
+                username: "test",
+                password: "test",
+                name: "test" 
+            })
+
+        console.log(response)
+        expect(response.status).toBe(200)
+        expect(response.body.data.username).toBe("test")
+        expect(response.body.data.name).toBe("test")
     })
 })
